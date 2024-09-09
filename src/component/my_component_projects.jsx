3d223303d81895426187935769062c0626bc1937
unknown <wassouf.ryan@outlook.fr>
2024-09-09 13:41:49 +0200

    first commit

---
+++ src/component/my_component_projects.jsx
@@ -0,0 +1,7 @@
+export default function MyComponentProjects(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import MyComponentHome from '../component/my_component_home';
+import '../styles/style.css';
+
+export default function Home() {
+    return (
+        <>
+            <MyComponentHome />
+        </>
+    )
+}
